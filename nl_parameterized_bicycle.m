4fb87c48ca75f6c6d84eef681475dca68b5c1856
Jason Moore <moorepants@gmail.com>
2011-10-10 14:42:56 -0700

    Abandoning the non linear grey box model.

--- nl_parameterized_bicycle.m
+++ nl_parameterized_bicycle.m
@@ -1,4 +1,4 @@
-function [dx, y] = parameterized_bicycle(t, x, u, kDelta, kPhiDot, kPhi, kPsi, kY, fnm, varargin)
+function [dx, y] = nl_parameterized_bicycle(t, x, u, kDelta, kPhiDot, kPhi, kPsi, kY, fnm, varargin)
 
 gains = [kDelta, kPhiDot, kPhi, kPsi, kY];
 
@@ -16,6 +16,8 @@ B = data.system.B;
 C = data.system.C;
 D = data.system.D;
 outputs = data.system.outputs;
+
+keepers = {'Delta', 'DeltaDot', 'Phi', 'PhiDot', 'Psi', 'PsiDot', 'Y', 'TDelta'};
 
 dx = A * x + B * u;
 y = C * x + D * u;
