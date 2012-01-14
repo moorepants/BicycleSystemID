232620d635acadf80990c39485d2e7df4bc2c49d
Jason Moore <moorepants@gmail.com>
2012-01-14 12:48:54 -0800

    bicycle_structured now accepts a seed bicycle and speed.

--- bicycle_structured.m
+++ bicycle_structured.m
@@ -1,11 +1,15 @@
-function grey = bicycle_structured(varargin)
-% function grey = bicycle_structured(varargin)
+function grey = bicycle_structured(bicycle, speed, varargin)
+% function grey = bicycle_structured(bicycle, speed, varargin)
 %
 % Returns a structured idss model of the Whipple model linearized about the
 % nominal configuration.
 %
 % Parameters
 % ----------
+% bicycle : char
+%   The name of the bicycle.
+% speed : double
+%   The speed of the bicycle.
 % varargin : char/cell array pairs, optional
 %   Specify a subset of states, inputs or outputs by setting one of the
 %   following: `states`, `inputs`, `outputs` as a cell array of
@@ -30,21 +34,22 @@ addpath(PATH_TO_CONTROL_MODEL)
 
 % The following definition of the structured model could potentially set a
 % an entry in the A, B, C, D matrices that should be free to structured
-% because in only checks for zeros and ones. It would be smarter to
-% explicity define the nan for every entry in the model based on the known
-% results. This is the quicker way, that should work 99% of the time (or
-% more).
+% because in only checks for zeros and ones. There is the possibily that
+% with a certain combination of parameters one of the entries would equaly
+% zero or one. It would be smarter to explicity define the nan for every
+% entry in the model based on the known results. This is the quicker way,
+% that should work 99% of the time (or more).
 
-% generate the bicycle model at a random speed for the base model
-bicycle = bicycle_state_space('Benchmark', 0.398, varargin{:});
+% generate the bicycle model at a random stable speed for the base model
+bicycleSS = bicycle_state_space(bicycle, speed, varargin{:});
 
-grey = idss(bicycle, 'SSParameterization', 'Structured');
+grey = idss(bicycleSS, 'SSParameterization', 'Structured');
 
 % for every entry in A, B, C, D that is not zero or 1, set the structured to
 % nan
 for m = {'A', 'B', 'C', 'D'}
-    mat = bicycle.(m{:});
-    matS = bicycle.(m{:});
+    mat = bicycleSS.(m{:});
+    matS = bicycleSS.(m{:});
     % finds all non zero entries
     nonZero = abs(mat) > 1e-10;
     % finds all non one entries
