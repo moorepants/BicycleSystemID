911c5433f6261378fcdf2ff187a6b716bac5d401
Jason Moore <moorepants@gmail.com>
2012-01-23 17:03:37 -0800

    Added the arms model to the results for comparison.

--- scripts/verify-whipple.py
+++ scripts/verify-whipple.py
@@ -1,63 +1,85 @@
-# This is an attempt to use the measured steer torque input from a run with no
-# external disturbances and see if the Whipple model predicts the steer and
-# roll.
+#!/usr/bin/env python
+
+# This script generates the time history of the steer and roll torques given
+# the steer/roll coordinates, rates and accelerations for both the Whipple
+# model and the Whipple model with the inertial effects of the arms and
+# compares it to the measured steer torque.
+
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
-from uncertainties import unumpy
-import bicycleparameters as bp
 from bicycledataprocessor import main as bdp
 
-dataset = bdp.DataSet(fileName='../BicycleDataProcessor/InstrumentedBicycleData.h5')
+# load the experimental data
+dataset = bdp.DataSet(fileName='../../BicycleDataProcessor/InstrumentedBicycleData.h5')
 dataset.open(mode='r')
 
-pathToPar = '../BicycleParameters/data'
+pathToPar = '../../BicycleParameters/data'
 
-run = bdp.Run('00644', dataset.database, pathToPar, filterFreq=30.0)
+trial = bdp.Run('00638', dataset.database, pathToPar, filterFreq=30.0)
 
-lukeRigid = bp.Bicycle('Rigidcl', pathToData=pathToPar, forceRawCalc=True)
-lukeRigid.add_rider('Luke', reCalc=True)
+speed = trial.taskSignals['ForwardSpeed'][50:-50].mean()
 
-# roll rate, steer rate, roll angle, steer angle
-# roll torque, steer torque
-A, B = lukeRigid.state_space(4.0)
+steerTorque = trial.taskSignals['SteerTorque'].subtract_mean()
 
-A = unumpy.nominal_values(A)
-B = unumpy.nominal_values(B)
-#C = np.eye(4)
-#D = np.zeros((4, 2))
-C = np.array([1., 0., 0., 0.])
-D = np.array([0., 0.])
+rollAngle = trial.taskSignals['RollAngle'].subtract_mean()
+steerAngle = trial.taskSignals['SteerAngle'].subtract_mean()
+q = np.vstack((rollAngle, steerAngle))
 
-steerTorque = run.taskSignals['SteerTorque']
-rollTorque = np.zeros_like(steerTorque)
-u = np.vstack((rollTorque, steerTorque))
+rollRate = trial.taskSignals['RollRate'].subtract_mean()
+steerRate = trial.taskSignals['SteerRate'].subtract_mean()
+qd = np.vstack((rollRate, steerRate))
 
-#whipple = signal.lti(A, B, C, D)
+rollAccel = rollRate.time_derivative().filter(30.).subtract_mean()
+steerAccel = steerRate.time_derivative().filter(30.).subtract_mean()
+qdd = np.vstack((rollAccel, steerAccel))
 
-rollRate = run.taskSignals['RollRate']
-steerRate = run.taskSignals['SteerRate']
-rollAngle = run.taskSignals['RollAngle']
-steerAngle = run.taskSignals['SteerAngle']
+# compute the inputs from the Whipple model
+lukeRigid = trial.bicycle
 
-x0 = np.array([rollRate[0],
-               steerRate[0],
-               rollAngle[0],
-               steerAngle[0]])
+M, C1, K0, K2 = lukeRigid.canonical(nominal=True)
+
+C = speed * C1
+K = K0 * trial.bicycleRiderParameters['g'] + K2 * speed ** 2
+
+u = np.dot(M, qdd) + np.dot(C, qd) + np.dot(K, q)
+
+# compute the inputs from the Whipple model with rider arms
+# this is only valid for run 638 (i.e. Luke on Rigidcl at the measured speed)
+
+armA = np.array([[8.7171, -18.8791, -0.0370, -1.4646], # phi
+                 [4.3115, -1.5062, 2.4852, -7.0466]]) # delta
+# rows: phi, delta, columns: Tphi, Tdelta
+armB = np.array([[0.0081, -0.0241],
+                 [-0.0241, 1.6196]])
+xd = qdd
+x = np.vstack((q, qd))
+
+armU = np.dot(np.linalg.inv(armB), (xd - np.dot(armA, x)))
+
+# plot the results
+fig = plt.figure()
 
 time = steerTorque.time()
 
-#ysim = signal.lsim2(whipple, u, time, x0)
-tsim, ysim, xsim = signal.lsim((A, B, C, D), u.T, time, X0=x0)
+ax1 = fig.add_subplot(2, 1, 1)
+ax1.plot(time, u[0, :],
+         time, armU[0, :])
+ax1.set_ylabel('Roll Torque [Nm]')
+ax1.set_xlabel('Time [s]')
+ax1.legend(('Whipple Model Prediction', 'Arm Model Prediction'))
+leg1 = ax1.get_legend()
+plt.setp(leg1.get_texts(), fontsize='small')
 
-fig = plt.figure()
-ax = fig.add_subplot(4, 1, 1)
-ax.plot(time, ysim, time, rollRate)
-ax = fig.add_subplot(4, 1, 2)
-#ax.plot(time, ysim[1], time, steerRate)
-#ax = fig.add_subplot(4, 1, 3)
-#ax.plot(time, ysim[2], time, rollAngle)
-#ax = fig.add_subplot(4, 1, 4)
-#ax.plot(time, ysim[3], time, steerAngle)
+ax2 = fig.add_subplot(2, 1, 2)
+ax2.plot(time, steerTorque, 'k',
+         time, u[1, :],
+         time, armU[1, :])
+ax2.set_ylabel('Steer Torque [Nm]')
+ax2.set_xlabel('Time [s]')
+ax2.legend(('Experimental Measurment', 'Whipple Model Prediction',
+    'Arm Model Prediction'))
+leg2 = ax2.get_legend()
+plt.setp(leg2.get_texts(), fontsize='x-small')
+
+fig.savefig('../plots/torque-comparison.png')
 fig.show()
-
