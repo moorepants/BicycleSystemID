8045af9e33d71938cb12e4731775bb248074bfb5
Jason Moore <moorepants@gmail.com>
2012-04-02 17:56:33 -0700

    Push a ton of this to functions in csi.

--- scripts/fit_benchmark_canon.py
+++ scripts/fit_benchmark_canon.py
@@ -1,12 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import bicycledataprocessor as bdp
+#!/usr/bin/env python
+import cPickle
 import canonical_system_id as csi
-from dtk import bicycle
-
 
 riders = ['Charlie', 'Jason', 'Luke']
-#riders = ['Luke']
 environments = ['Horse Treadmill', 'Pavillion Floor']
 maneuvers = ['Balance',
              'Track Straight Line',
@@ -17,109 +13,77 @@ runs = csi.select_runs(riders, maneuvers, environments)
 
 # This gives the proportion of the lateral force which should be added to the
 # steer torque and roll torque equations in the canonical equations.
-H = {}
-for rider in riders:
-    A, B, F = csi.whipple_state_space(rider, 1.0)
-    H[rider] = np.dot(np.linalg.inv(B[2:]), F[2:])
+H = csi.lateral_force_contribution(riders)
 
-#rollParams = ['Mpp', 'Mpd', 'C1pp', 'C1pd',
-               #'K0pp', 'K0pd', 'K2pp', 'K2pd'] # all
-#rollParams = ['Mpp', 'Mpd', 'C1pd', 'K0pp', 'K0pd', 'K2pd'] # all but zeros
-#rollParams = ['C1pd', 'K0pp', 'K0pd', 'K2pd'] # all but zeros and mass matrix
+# try to load in all of the runs for the given run numbers.
+trials, errors = csi.load_trials(runs, H)
+
 rollParams = ['Mpd', 'C1pd', 'K0pd']
-
-trials = {}
-errors = []
-dataset = bdp.DataSet()
-
-for i, r in enumerate(runs):
-    try:
-        trial = bdp.Run(r, dataset, filterFreq=15.)
-    except bdp.bdpexceptions.TimeShiftError:
-        errors.append(r)
-    except IndexError:
-        errors.append(r)
-    else:
-        trials[r] = trial
-
-for runNum, trial in trials.items():
-    timeSeries = csi.benchmark_time_series(trial, subtractMean=True)
-    M, C1, K0, K2 = trial.bicycle.canonical(nominal=True)
-    fixedValues = csi.benchmark_canon_to_dict(M, C1, K0, K2,
-            H[trial.metadata['Rider']])
-    rollA, rollB = csi.benchmark_lstsq_matrices(rollParams, timeSeries, fixedValues)
-
-    try:
-        totRollA = np.vstack((totRollA, rollA))
-        totRollB = np.hstack((totRollB, rollB))
-    except NameError:
-        totRollA = rollA
-        totRollB = rollB
-
-rollSol = np.linalg.lstsq(totRollA, totRollB)[0]
-
-#steerParams = ['Mdp', 'Mdd', 'C1dp', 'C1dd',
-                #'K0dp', 'K0dd', 'K2dp', 'K2dd'] # all
-#steerParams = ['Mdp', 'Mdd', 'C1dp', 'C1dd', 'K0dp', 'K0dd', 'K2dd'] # all but zeros
-#steerParams = ['C1dp', 'C1dd', 'K0dp', 'K0dd', 'K2dd'] # all but zeros and mass matrix
-#steerParams = ['Mdp', 'Mdd', 'C1dp', 'C1dd',
-                #'K0dp', 'K0dd', 'K2dd']
 steerParams = ['Mdd', 'C1dp', 'C1dd',
-                'K0dd', 'K2dd', 'HdF']
+               'K0dd', 'K2dd', 'HdF']
+canon = csi.load_benchmark_canon(riders)
 
-for runNum, trial in trials.items():
-    timeSeries = csi.benchmark_time_series(trial, subtractMean=True)
-    M, C1, K0, K2 = trial.bicycle.canonical(nominal=True)
-    M_mod = M.copy()
-    K0_mod = K0.copy()
-    M_mod[1, 0] = rollSol[rollParams.index('Mpd')]
-    K0_mod[1, 0] = rollSol[rollParams.index('K0pd')]
-    fixedValues = csi.benchmark_canon_to_dict(M_mod, C1, K0_mod, K2,
-            H[trial.metadata['Rider']])
-    #fixedValues['Mdp'] = rollSol[rollParams.index('Mpd')]
-    #fixedValues['K0dp'] = rollSol[rollParams.index('K0pd')]
-    steerA, steerB = csi.benchmark_lstsq_matrices(steerParams, timeSeries, fixedValues)
-    try:
-        totSteerA = np.vstack((totSteerA, steerA))
-        totSteerB = np.hstack((totSteerB, steerB))
-    except NameError:
-        totSteerA = steerA
-        totSteerB = steerB
+idMatrices = {}
 
-steerSol = np.linalg.lstsq(totSteerA, totSteerB)[0]
+runs = list(set(runs).difference(errors))
+means = csi.mean_canon(riders, canon, H)
+idMat = csi.enforce_symmetry(runs, trials, rollParams,
+        steerParams, *means)
+idMatrices['All'] = idMat
 
-canon = (M, C1, K0, K2)
-M_id, C1_id, K0_id, K2_id, H_id = csi.benchmark_identified_matrices((rollParams,
-    steerParams), (rollSol, steerSol), M_mod, C1, K0_mod, K2, H[trial.metadata['Rider']])
+for rider in riders:
+    idMatrices[rider] = {}
 
-speeds = np.linspace(0, 10, num=50)
-eVals = np.zeros((len(speeds), 4), dtype=np.complex)
-for i, v in enumerate(speeds):
-        A, B = bicycle.abMatrix(M_id, C1_id, K0_id, K2_id, v, 9.8)
-        eVals[i] = np.linalg.eig(A)[0]
+    # this find the model for each rider in both environments
+    runs = csi.select_runs([rider], maneuvers, environments)
+    runs = list(set(runs).difference(errors))
 
-eVals2 = np.zeros((len(speeds), 4), dtype=np.complex)
-for i, v in enumerate(speeds):
-    A, B = bicycle.abMatrix(M, C1, K0, K2, v, 9.8)
-    eVals2[i] = np.linalg.eig(A)[0]
+    means = csi.mean_canon(riders, canon, H)
+    idMat = csi.enforce_symmetry(runs, trials, rollParams,
+            steerParams, *means)
+    idMatrices[rider]['All'] = idMat
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.plot(speeds, eVals2, '.k', speeds, eVals, '.b')
-ax.set_ylim((-10, 10))
+    # this finds the model for each rider in each environment
+    for env in environments:
+        runs = csi.select_runs([rider], maneuvers, [env])
+        runs = list(set(runs).difference(errors))
 
-print 'M'
-print M
-print M_id
-print 'C1'
-print C1
-print C1_id
-print 'K0'
-print K0
-print K0_id
-print 'K2'
-print K2
-print K2_id
-print 'H'
-print H[trial.metadata['Rider']]
-print H_id
+        means = csi.mean_canon(riders, canon, H)
+        idMat = csi.enforce_symmetry(runs, trials, rollParams,
+                steerParams, *means)
+        idMatrices[rider][env] = idMat
+
+# for each environment calculate the model for all riders
+for env in environments:
+    runs = csi.select_runs(riders, maneuvers, [env])
+    runs = list(set(runs).difference(errors))
+
+    means = csi.mean_canon(riders, canon, H)
+    idMat = csi.enforce_symmetry(runs, trials, rollParams,
+            steerParams, *means)
+    idMatrices[env] = idMat
+
+# save all of the identified matrices to file
+with open('idMatrices.p', 'w') as f:
+    cPickle.dump(idMatrices, f)
+
+#print 'M'
+#print M
+#print M_id
+#print abs(M_id - M) / M * 100
+#print 'C1'
+#print C1
+#print C1_id
+#print abs(C1_id - C1) / C1 * 100
+#print 'K0'
+#print K0
+#print K0_id
+#print abs(K0_id - K0) / K0 * 100
+#print 'K2'
+#print K2
+#print K2_id
+#print abs(K2_id - K2) / K2 * 100
+#print 'H'
+#print HMean
+#print H_id
+#print abs(H_id - HMean) / HMean * 100
