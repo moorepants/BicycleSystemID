bec22c476059ce8ded6b8044cd10ce953119dc77
Jason Moore <moorepants@gmail.com>
2012-04-02 18:25:12 -0700

    Added doc string for guess_nearby.

--- guess_nearby.m
+++ guess_nearby.m
@@ -1,4 +1,21 @@
 function guesses = guess_nearby(seedGuess, numGuesses)
+% function guesses = guess_nearby(seedGuess, numGuesses)
+%
+% Returns a series of random guesses normally distributed around a seed
+% guess.
+%
+% Parameters
+% ----------
+% seedGuess : double, size(6)
+%   A guess for the  six free parameters in the control system
+%   identification.
+% numGuesses : integer
+%   The number of guesses to return.
+%
+% Returns
+% -------
+% guesses : double, size(numGuesses, 6)
+%   A series of random guesses.
 
 guesses = zeros(numGuesses, 6);
 for i = 1:length(numGuesses)
